3ac744dff70ba13a575788157b53e2e972060642
akashsy05 <akashsy05@gmail.com>
2024-06-23 00:58:33 +0530

    ghj

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URI = 'file-sharing-is-easy.vercel:8000';
+const API_URI = "file-sharing-is-easy.vercel:8000";
 
 export const uploadFile = async (data) => {
-    try {
-        const response = await axios.post(`${API_URI}/upload`, data);
-        return response.data;
-    } catch (error) {
-        console.log('Error while calling the API ', error.message);
-    }
-}
+  try {
+    const response = await axios.post(`${API_URI}/upload`, data);
+    return response.data;
+  } catch (error) {
+    console.log("Error while calling the API ", error.message);
+  }
+};
